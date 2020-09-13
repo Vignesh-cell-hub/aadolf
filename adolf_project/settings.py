ebeb0f964c0aa4740e7d00bae203b3bb8b5016f0
Vicky <jpvignesh2001@gmail.com>
2020-09-13 10:22:34 +0530

    setting change

--- adolf_project/settings.py
+++ adolf_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'mq&=2s6i_@#ydov)ik1&rc8!dqm@algbh&&b($l9mcv&&no$ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['aadolf-ledger.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['aadolf-ledger.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
