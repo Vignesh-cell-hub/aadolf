7bb43a9a7e86bfe5b0ff7e492deccb4acfd532f5
venky <venkatesh.vallis@gmail.com>
2020-09-14 22:26:06 +0530

    edit funtions for vendor,customer,expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('upload/',views.upload,name="upload")
+    path('',views.index,name="expense_index"),
+    path('upload/',views.upload,name="expense_upload"),
+    path('<int:id>',views.editexpense,name="edit_expense"),
 ]
--- vendor/urls.py
+++ vendor/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('upload/',views.upload,name="upload")
+    path('',views.index,name="vendor_index"),
+    path('upload/',views.upload,name="vendor_upload"),
+    path('<int:id>',views.editvendor,name="edit_vendor"),
 ]
