caa3195cde8c218868892bb9938021b210f76a26
vignesh <jpvignesh2001@gmail.com>
2020-09-29 20:29:18 +0530

    tbl and form chng

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('',views.index,name="dashboard_index"),
     #path('upload/',views.upload,name="upload")
 ]
--- estimate/urls.py
+++ estimate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('',views.index,name="estimate_index"),
     path('getdata/<int:id>/',views.getdata,name="getdata"),
     path('upload/',views.upload,name="upload")
 ]
